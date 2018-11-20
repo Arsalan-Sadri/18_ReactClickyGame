79c9456b45137173a2584ab6829b1999999832d7
Arsalan Sadri <asadri62@gmail.com>
2018-11-19 23:43:21 -0800

    Minor changes

--- src/App.js
+++ src/App.js
@@ -5,139 +5,130 @@ import axi from "./data";
 import "./App.css";
 
 class App extends Component {
-  
-  setChildsStates = [];
-  
-  state = {
-    photosArr: [],
-    score: 0,
-    topScore: 0,
-    animation: false,
-    showMsg: false
-  };
+    setChildsStates = [];
 
-  restartAnim = () => {
-
-    this.setState({
-      animation: false
-    });
-
-    setTimeout(() => {
-
-      this.setState({
-        animation: true
-      });
-    }, 0);
-  }
-
-  toggleMsg = () => {
-
-    this.setState({
-      showMsg: true
-    });
-
-    setTimeout(() => {
-      this.setState({
+    state = {
+        photosArr: [],
+        score: 0,
+        topScore: 0,
+        animation: false,
         showMsg: false
-      });
-    }, 2000);
+    };
 
-  }
-
-  resetScore = () => {
-
-    this.toggleMsg();
-
-    this.restartAnim();
-
-    this.setState({
-      score: 0
-    });
-
-    this.setChildsStates.forEach((ref) => ref());
-
-    this.shuffleCards();
-  }
-
-  componentDidMount() {
-    axi.then(res => {
+    restartAnim = () => {
         this.setState({
-          photosArr: res.data.data
+            animation: false
         });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
 
-  incrementScore = (setChildState) => {
+        setTimeout(() => {
+            this.setState({
+                animation: true
+            });
+        }, 0);
+    };
 
-    if (this.state.topScore > this.state.score)
-      this.setState({
-        score: this.state.score + 1,
-      });
+    toggleMsg = () => {
+        this.setState({
+            showMsg: true
+        });
 
-    else this.setState({
-      score: this.state.score + 1,
-      topScore: this.state.topScore + 1
-    });
+        setTimeout(() => {
+            this.setState({
+                showMsg: false
+            });
+        }, 2000);
+    };
 
-    this.setChildsStates.push(setChildState);
-    this.shuffleCards();
-  }
+    resetScore = () => {
+        this.toggleMsg();
 
-  shuffleCards = () => {
-    this.setState({
-      photosArr: this.shuffleArr(this.state.photosArr)
-    });
-  };
+        this.restartAnim();
 
-  shuffleArr(arr) {
-    var j, x, i;
-    for (i = arr.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = arr[i];
-      arr[i] = arr[j];
-      arr[j] = x;
+        this.setState({
+            score: 0
+        });
+
+        this.setChildsStates.forEach(ref => ref());
+
+        this.shuffleCards();
+    };
+
+    componentDidMount() {
+        axi.then(res => {
+            this.setState({
+                photosArr: res.data.data
+            });
+        }).catch(err => {
+            console.log(err);
+        });
     }
-    return arr;
-  }
 
-  render() {
-    
-    return (
-        <div>
-          <div className="row header">
-              <div className="logo col-sm">
-                  <h1>Memory</h1>
-                  <h1>Game</h1>
-              </div>
-              <div className="msg col-sm">
-                  <h1 className={this.state.animation? "anim": ""}>
-                  {this.state.showMsg? "Wrong choice!": ""}
-                  </h1>
-              </div>
-              <div className="score col-sm">
-                  <h1>Score: {this.state.score}</h1>
-                  <h1>Top score: {this.state.topScore}</h1>
-              </div>
-          </div>
-            <div className={this.state.animation? "anim": ""}>
-                <Wrapper>
-                  {this.state.photosArr.map(photo => (
-                    <Card
-                      key={photo.id}
-                      id={photo.id}
-                      photoURL={photo.images.fixed_height_still.url}
-                      incrementScore={this.incrementScore}
-                      resetScore={this.resetScore}
-                    />
-                  ))}
-                </Wrapper>
+    incrementScore = setChildState => {
+        if (this.state.topScore > this.state.score)
+            this.setState({
+                score: this.state.score + 1
+            });
+        else
+            this.setState({
+                score: this.state.score + 1,
+                topScore: this.state.topScore + 1
+            });
+
+        this.setChildsStates.push(setChildState);
+        this.shuffleCards();
+    };
+
+    shuffleCards = () => {
+        this.setState({
+            photosArr: this.shuffleArr(this.state.photosArr)
+        });
+    };
+
+    shuffleArr(arr) {
+        var j, x, i;
+        for (i = arr.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = arr[i];
+            arr[i] = arr[j];
+            arr[j] = x;
+        }
+        return arr;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="row header">
+                    <div className="logo col-sm">
+                        <h1>Memory</h1>
+                        <h1>Game</h1>
+                    </div>
+                    <div className="msg col-sm">
+                        <h1 className={this.state.animation ? "anim" : ""}>
+                            {this.state.showMsg ? "Wrong choice!" : ""}
+                        </h1>
+                    </div>
+                    <div className="score col-sm">
+                        <h1>Score: {this.state.score}</h1>
+                        <h1>Top score: {this.state.topScore}</h1>
+                    </div>
+                </div>
+                <div className={this.state.animation ? "anim" : ""}>
+                    <Wrapper>
+                        {this.state.photosArr.map(photo => (
+                            <Card
+                                key={photo.id}
+                                id={photo.id}
+                                photoURL={photo.images.fixed_height_still.url}
+                                incrementScore={this.incrementScore}
+                                resetScore={this.resetScore}
+                            />
+                        ))}
+                    </Wrapper>
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,41 +2,38 @@ import React from "react";
 import "./Card.css";
 
 class Card extends React.Component {
-
-  state = {
-    selected: false
-  }
-
-  setMyState = () => {
-    this.setState({
-      selected: false
-    });
-  };
-
-  cardSelectHandler = () => {
-    if (!this.state.selected) {
-      this.setState({
-        selected: true
-      });
-      this.props.incrementScore(this.setMyState);
-    } 
-    else {
-      this.setState({
+    state = {
         selected: false
-      });
-      this.props.resetScore();
+    };
+
+    setMyState = () => {
+        this.setState({
+            selected: false
+        });
+    };
+
+    cardSelectHandler = () => {
+        if (!this.state.selected) {
+            this.setState({
+                selected: true
+            });
+            this.props.incrementScore(this.setMyState);
+        } else {
+            this.setState({
+                selected: false
+            });
+            this.props.resetScore();
+        }
+    };
+
+    render() {
+        return (
+            <div className="card" onClick={this.cardSelectHandler}>
+                <div className="img-container">
+                    <img alt={this.props.id} src={this.props.photoURL} />
+                </div>
+            </div>
+        );
     }
-  };
-
-  render () {
-    return (
-      <div className="card" onClick={this.cardSelectHandler}>
-        <div className="img-container">
-          <img alt={this.props.id} src={this.props.photoURL} />
-        </div>
-      </div>
-    );
-  }
-
 }
 export default Card;
