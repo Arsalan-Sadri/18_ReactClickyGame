1c0ddf54a855bcb228d4f515e576f90e5c7d9d93
Arsalan Sadri <asadri62@gmail.com>
2019-03-05 14:57:22 -0800

    Updates

---
+++ precache-manifest.f05dda96b7083a748132988d0e9310fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "839068dbe11444743a2a",
+    "url": "/18_ReactClickyGame/static/js/runtime~main.839068db.js"
+  },
+  {
+    "revision": "6870661e2bcc86efea48",
+    "url": "/18_ReactClickyGame/static/js/main.6870661e.chunk.js"
+  },
+  {
+    "revision": "6f9879512bc00057a3db",
+    "url": "/18_ReactClickyGame/static/js/1.6f987951.chunk.js"
+  },
+  {
+    "revision": "6870661e2bcc86efea48",
+    "url": "/18_ReactClickyGame/static/css/main.6e920ca4.chunk.css"
+  },
+  {
+    "revision": "5558778dea190762fb5befb59e33b7e2",
+    "url": "/18_ReactClickyGame/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/18_ReactClickyGame/precache-manifest.f05dda96b7083a748132988d0e9310fc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/18_ReactClickyGame/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
